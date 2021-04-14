9d136fd3c6174223e17af47c55b8bd0f96006da1
Mike Santos <mike.santos@contraprova.com.br>
2021-04-14 11:38:29 -0300

    configurando as rotas do projeto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import Routes from './Routes';
 
 function Page(){
     const dispatch = useDispatch();
     const email = useSelector(state=>state.user.email);
     return(
-        <>
-            <h1>Olá, Mundo!</h1>
-            <p>E-mail: {email}</p>
-        </>
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
     );
 }
 
---
+++ src/pages/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Page = () => {
+    return(
+        <div>
+            <h1>Página Sobre</h1>
+            <Link to="/">Home</Link>
+        </div>
+    );
+}
+
+export default Page;
