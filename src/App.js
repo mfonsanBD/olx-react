3008ee15185e971dc18beaf8bb380ff5170fe9ff
Mike Santos <mike.santos@contraprova.com.br>
2021-04-14 12:46:54 -0300

    estruturação de arquivos do projeto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+// import {useSelector, useDispatch} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import Routes from './Routes';
+import {Template} from './components/MainComponents';
+import Header from './components/partials/Header';
+import Footer from './components/partials/Footer';
 
 function Page(){
-    const dispatch = useDispatch();
-    const email = useSelector(state=>state.user.email);
+    // const dispatch = useDispatch();
+    // const email = useSelector(state=>state.user.email);
     return(
         <BrowserRouter>
-            <Routes/>
+            <Template>
+                <Header/>
+                    <Routes/>
+                <Footer/>
+            </Template>
         </BrowserRouter>
     );
 }
---
+++ src/components/partials/Footer/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const FooterArea = styled.div`
+    margin-top:20px;
+    height:100px;
+    background-color:#CCC;
+`;
