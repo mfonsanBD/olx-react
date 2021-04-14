ad1e9189ace2b62206dc9b997ae45d1a11fec9b7
Mike Santos <mike.santos@contraprova.com.br>
2021-04-14 12:58:03 -0300

    definindo estilo padrao para o App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import {useSelector, useDispatch} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
+import "./App.css";
 import Routes from './Routes';
 import {Template} from './components/MainComponents';
 import Header from './components/partials/Header';
